4b2ac0a1d24ddf39ec2c85f6e751d7fca5873d6f
Santhosh Janakiraman <santhosh30499@gmail.com>
2025-12-03 22:15:39 -0500

    minor changes to long experiments

---
+++ run_long_dp_epsilon_8.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEVICE="cuda:0"
+
+python3 dp_grokking_experiments.py \
+--device "$DEVICE" \
+--binary_operation="add_mod" \
+--num_epochs=10000 \
+--train_fraction=0.4 \
+--log_frequency=500 \
+--lr=5e-4 \
+--adam_epsilon=1e-30 \
+--cross_entropy_dtype="float16" \
+--use_dp \
+--target_epsilon=8.0 \
+--max_grad_norm 1.0
